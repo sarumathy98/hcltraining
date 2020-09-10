46ed9ffb24fbdc07fdfa20157696bf67e9b282f9
sarumathy98 <68985325+sarumathy98@users.noreply.github.com>
2020-09-10 22:00:52 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,36 +1,63 @@
-package com.blower;
+package com.hcl;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Long over,balls,runs;
-		String  batsman_name,bowler_name,nonstriker_name;
-		 Delivery d=new Delivery();
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the over");
-		over=sc.nextLong();
-		System.out.println("Enter the ball");
-		balls=sc.nextLong();
-		System.out.println("Enter the runs");
-		 runs=sc.nextLong();
-		 System.out.println("Enter the batsman name");
-		 batsman_name=sc.nextLine();
-         System.out.println("Enter the bowler name");
-         bowler_name=sc.nextLine();
-         System.out.println("Enter the nonstriker_name");
-         nonstriker_name=sc.nextLine();
-        
-         d.setOver(over);
-         d.setBalls(balls);
-         d.setRuns(runs);
-         d.setBatsman_name(batsman_name);
-         d.setBowler_name(bowler_name);
-         d.setNonstriker_name(nonstriker_name);
-         d.displayDeliveryDetails();
-     
-		
+		Scanner sc= new Scanner(System.in);
+		System.out.println("choose stall type:");
+		System.out.println("1.Gold Stall");
+		System.out.println("2.Premium Stall");
+		System.out.println("3.Executive Stall");
+		int choice;
+		choice=sc.nextInt();
+	    if(choice==1)
+	    {
+	    	System.out.println("Enter the stall details");
+	        System.out.println("Stall Name: ");
+	        String stall_name=sc.nextLine();
+	        System.out.println("cost: ");
+	        int cost=sc.nextInt();
+	        System.out.println("Owner Name:");
+	        String own_name=sc.nextLine();
+	        System.out.println("Number of Screen");
+	        int num_screen=sc.nextInt();
+	        Stall s=new GoldStall(stall_name,own_name,cost,num_screen);
+	        s.display();
+	    }
+	        else if(choice==2)
+	        {
+	        	System.out.println("Enter the stall details");
+	            System.out.println("stallname:");
+	            String stall_name=sc.nextLine();
+	            System.out.println("cost:");
+	            int cost=sc.nextInt();
+	            System.out.println("ownername:");
+	            String own_name=sc.nextLine();
+	            System.out.println("sets:");
+	            int projector=sc.nextInt();
+	            Stall s=new PremiumStall(stall_name,own_name,cost,projector);
+	            s.display();
+	        }
+	        else if(choice==3)
+	        {
+	        	System.out.println("Enter the stall details");
+	            System.out.println("Stall Name:");
+	            String stall_name=sc.nextLine();
+	            System.out.println("cost:");
+	            int cost=sc.nextInt();
+	            System.out.println("Owner Name:");
+	            String own_name=sc.nextLine();
+	            System.out.println("Number of Screen");
+	            int tv_screen=sc.nextInt();
+	            Stall s=new ExecutiveStall(stall_name,own_name,cost,tv_screen);
+	            s.display();
+	        }
+
+	    }
+	
+
 
 	}
 
-}
+
