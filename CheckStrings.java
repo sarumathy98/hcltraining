dce2d8c562130b70f861243386dfc37b4f5506fb
sarumathy98 <68985325+sarumathy98@users.noreply.github.com>
2020-09-11 21:40:30 +0530

    Add files via upload

---
+++ CheckStrings.java
@@ -0,0 +1,21 @@
+package com.kkk;
+import java.util.*;
+import java.io.*;
+
+
+public class CheckStrings {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Humpty's sentence");
+	    String s=sc.nextLine();
+	    System.out.println("Enter Dumpty's sentence");
+	    String s1=sc.nextLine();
+	    String s2=s + s1;
+	    System.out.println("Concantened String : "+s2);
+
+		
+
+	}
+
+}
