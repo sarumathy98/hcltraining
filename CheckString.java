909ce387c16eb959c1fff3c69f9a2550ebbc731a
sarumathy98 <68985325+sarumathy98@users.noreply.github.com>
2020-09-11 21:42:07 +0530

    Add files via upload

--- CheckString.java
+++ CheckString.java
@@ -1,27 +1,19 @@
 package com.kk;
-import java.io.*;
-import java.lang.*;
+import java.util.*;
 
-import java.util.Scanner;
 
 public class CheckString {
 
 	public static void main(String[] args) {
-			Scanner sc=new Scanner(System.in);
-			String s=sc.nextLine();
-			int location;
-			System.out.println("Enter the location");
-			 location=sc.nextInt();
-			System.out.println("What humpty want to add?");
-			String s1=sc.nextLine();
-			StringBuffer s2=new StringBuffer(s);
-			s2.insert(location+1, s1);
-			System.out.println("Dumpty's new Statement: "+s);
-		}
-
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the humpty's statement");
+		String s=sc.nextLine();
+		StringBuffer s1=new StringBuffer(s);
+		System.out.println(s1.reverse());
+		
+		
 	
-
 
 	}
 
-
+}
