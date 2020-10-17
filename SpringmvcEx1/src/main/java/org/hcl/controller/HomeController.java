3769834aa09401470f653ee4b75924bb4f3313a9
sarumathy98 <68985325+sarumathy98@users.noreply.github.com>
2020-10-17 20:34:46 +0530

    Add files via upload

---
+++ SpringmvcEx1/src/main/java/org/hcl/controller/HomeController.java
@@ -0,0 +1,14 @@
+package org.hcl.controller;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String showHome()
+	{
+		return "Home";
+	}
+	
+	
+
+}
