2203e0b154233ebd2489b938173f003a277143b7
sarumathy98 <68985325+sarumathy98@users.noreply.github.com>
2020-10-17 20:29:48 +0530

    Add files via upload

---
+++ HAndson2/src/main/java/org/hcl/Main.java
@@ -0,0 +1,21 @@
+package org.hcl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext context= new ClassPathXmlApplicationContext("applicationContext.xml");
+		Employee emp= context.getBean(Employee.class,"emp");
+		emp.display();
+		Address add= emp.getAddress();
+		add.display();
+		
+	}
+
+
+
+
+	}
+
